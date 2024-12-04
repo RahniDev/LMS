705a4ff443893992ae589a74f084ed7b21033fde
RahniDev <rahni32@protonmail.com>
2024-12-04 17:24:35 +0100

    Create Next app, add Shadcn

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+const Home = () => {
+  return (
+    <>
+      <p className="text-3xl font-medium text-sky-700">LMS Website</p>
+    </>
+  );
+}
+
+export default Home
